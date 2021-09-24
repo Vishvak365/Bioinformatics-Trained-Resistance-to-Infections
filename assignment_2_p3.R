2d405ee795727f54e3fdeb207054cc331b099b47
Vishvak Seenichamy <vishvak365@gmail.com>
2021-09-24 16:04:14 -0400

    Create metadata and design data

--- assignment_2_p3.R
+++ assignment_2_p3.R
@@ -3,22 +3,36 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 
 BiocManager::install("SummarizedExperiment")
 BiocManager::install("DESeq2")
-#install.packages("BiocManager")
-install.packages("DESeq2")
-
-
-
-browseVignettes("DESeq2")
+install.packages("readxl")
+install.packages("tibble")
+install.packages("readr")
 
 #loading data
 library("readxl")
-
+library("DESeq2")
 #getting orig RNA counts and forcing data types
-RNA_counts_orig <- read_xlsx("RNA_counts_orig.xlsx", col_types=c("text","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric",
-                                                                 "numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric",
-                                                                 "numeric","numeric","numeric","numeric","numeric", "numeric"))
+RNA_counts_orig <- read_xlsx("RNA_counts_orig.xlsx", rowNames=TRUE)
+read.cs
+?read_xlsx
 
 
 #making it a dataframe and some renaming
 RNA_counts_orig_dataframe <- as.data.frame(RNA_counts_orig)
 names(RNA_counts_orig_dataframe)[1] <- "condition"
+RNA_counts_orig_dataframe
+columnData <- c("text","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric",
+                "numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric",
+                "numeric","numeric","numeric","numeric","numeric", "numeric")
+
+columnData
+inf_data <- c("infected", "infected","infected", "infected","infected", "infected",
+              "infected", "infected","infected", "infected","infected", "infected",
+              "infected", "control", "control","control", "control",
+              "control", "control","control", "control","control", "control",
+              "control", "control", "control")
+infected_data <- data.frame("infected_data"=inf_data)
+table(infected_data)
+dim(RNA_counts_orig_dataframe)
+ncol(RNA_counts_orig_dataframe)
+ddset <- DESeqDataSetFromMatrix(countData = RNA_counts_orig_dataframe, colData = infected_data, design = infected_data )
+?DESeqDataSetFromMatrix

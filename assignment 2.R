3d4b12d774e9e2ce87ab768cbe729c6924e55c6a
Justin Hui <75865779+justinhui0@users.noreply.github.com>
2021-09-30 17:23:51 -0400

    Part 1 and 2
    Parts 1 and 2, Loads data into R, PlotPCA/tsne/density plot

--- assignment 2.R
+++ assignment 2.R
@@ -11,14 +11,15 @@ RNA_counts_orig <- read_xlsx("RNA_counts_orig.xlsx", col_types=c("text","numeric
 RNA_counts_orig_dataframe <- as.data.frame(RNA_counts_orig)
 names(RNA_counts_orig_dataframe)[1] <- "condition"
 
-library("DESeq2")
-
 #summarizing data, each col needs a colData which is different from just the name of the column, its like a hidden label for each col
-summed <- SummarizedExperiment(RNA_counts_orig_dataframe[1:17977,2:27 ], colData= c("infected", "infected","infected", "infected","infected", "infected",
+summed <- SummarizedExperiment(RNA_counts_orig_dataframe[2:17977,2:27 ], colData= c("infected", "infected","infected", "infected","infected", "infected",
                                                                                     "infected", "infected","infected", "infected","infected", "infected",
-                                                                                   "infected", "control", "control","control", "control",
+                                                                                    "infected", "control", "control","control", "control",
                                                                                     "control", "control","control", "control","control", "control",
                                                                                     "control", "control", "control"))
+
+
+
 
 #compute gene expression ranges and plot density of ranges
 library(matrixStats)
@@ -29,23 +30,20 @@ gene_ranges <- gene_minmax[,2] - gene_minmax[,1]
 length(gene_ranges)
 gene_ranges
 
+
+#density plots
 plot(density(gene_ranges))
 plot(density(log2(gene_ranges)))
 
 #plotPCA of summed
 plotPCA(DESeqTransform(summed), intgroup = c("X"))
 
-
+#Tsne plot
 library(M3C)
-#tsne? 2 diff graphs and compare? why do they change every time???
-#tsne(summed.matrix(infected),perplex = 1, labels = c("infected"))
-#tsne(data.matrix(control),perplex = 3, labels = c("control"))
-
 tsne(RNA_counts_orig[,2:27], perplex=1, labels = c("infected", "infected","infected", "infected","infected", "infected",
                                                    "infected", "infected","infected", "infected","infected", "infected",
                                                    "infected", "control", "control","control", "control",
                                                    "control", "control","control", "control","control", "control",
                                                    "control", "control", "control"))
 
-#differential analysis...
-#prep data...
+#Volcano plot

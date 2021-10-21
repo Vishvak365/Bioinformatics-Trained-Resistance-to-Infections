4181f7be11449e541421770723da9f6f27151f1e
Justin Hui <75865779+justinhui0@users.noreply.github.com>
2021-10-21 15:22:00 -0400

    sankey plot for k means done,...
    just change kmeans to your clustering algo to get your sankey plot

--- assignment 3.R
+++ assignment 3.R
@@ -66,17 +66,17 @@ deseq_df
 #order by pvalue to get most variable genes, not right though, how to get most variable genes?
 deseq_df <- deseq_df[order(abs(-deseq_df$log2FoldChange), decreasing = TRUE),]
 #get first 5000 and select only gene name and pval
-dataset <- deseq_df[3]
+dataset <- deseq_df[c(1,3)]
 dataset
 dataset <- dataset %>% 
   mutate(condition = 
            case_when(log2FoldChange < 0 ~ "inf",
                      log2FoldChange > 0 ~ "control"))
 dataset
-dataset[1:100, c(1)]
+dataset[1:100, c(2)]
 
 get_dataset <- function(index=5000) {
-  toret <- dataset[1:index, c(1)]
+  toret <- dataset[1:index, c(2)]
   
   return (toret)
 }
@@ -85,44 +85,63 @@ gene10 <- get_dataset(10)
 gene100 <- get_dataset(100)
 gene1000 <- get_dataset(1000)
 gene10000 <- get_dataset(10000)
+gene5000 <- get_dataset(5000)
 
+#different k values
+clusters5000 <-  kmeans(gene5000, 5, iter.max = 25, nstart = 1)
+clusters5000$size
+clusters5000 <-  kmeans(gene5000, 10, iter.max = 25, nstart = 1)
+clusters5000$size
+clusters5000 <-  kmeans(gene5000, 15, iter.max = 25, nstart = 1)
+clusters5000$size
+clusters5000 <-  kmeans(gene5000, 3, iter.max = 25, nstart = 1)
+clusters5000$size
+
+#different gene numbers, 10, 100, 1000, 10000, replace with your clustering algorithm
 clusters10 <- kmeans(gene10, 5, iter.max = 25, nstart = 1)
 clusters100 <- kmeans(gene100, 5, iter.max = 25, nstart = 1)
 clusters1000 <- kmeans(gene1000, 5, iter.max = 25, nstart = 1)
 clusters10000 <- kmeans(gene10000, 5, iter.max = 25, nstart = 1)
 
+clusters10$size
+clusters100$size
+clusters1000$size
+clusters10000$size
+
 #install.packages("ggalluvial")
 library("ggalluvial")
 
+#creating sanky plot
+all10 <- c( clusters10$size)
+all100 <- c( clusters100$size)
+all1000 <- c( clusters1000$size)
+all10000 <- c( clusters10000$size)
+clusternum <- c( "1", "2", "3", "4", "5")
+type <- c("gene10", "gene10","gene10","gene10","gene10",
+          "gene100","gene100","gene100","gene100","gene100",
+          "gene1000","gene1000","gene1000","gene1000","gene1000",
+          "gene10000","gene10000","gene10000","gene10000","gene10000")
+scalar1 <- function(x) {x / sqrt(sum(x^2))}
+scalar1(clusters10$size)
+normalize(clusters10$size)
+freq <- c(clusters10$size/10, clusters100$size/100,clusters1000$size/1000, clusters10000$size/10000)
 
-clusters10
+#k = 5
+group <- c("1", "2", "3", "4", "5",
+           "1", "2", "3", "4", "5",
+           "1", "2", "3", "4", "5",
+           "1", "2", "3", "4", "5")
 
-ggplot(data = as.data.frame(clusters),
-       aes(axis1 = gene_Num, y = accuracy/3)) +
-  scale_x_discrete(limits = c("gene_Num"), expand = c(.02, .05)) +
-  geom_stratum(width = 1/12, fill = "black", color = "grey") +
-  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
-  ggtitle("Accuracy KNN on different number of genes")
+#plotting
+toplot <- data.frame(type, freq, group)
 
-clusters$size
+toplot
 
-#top ten significant genes
-heatmap_genes = deseq_df$log2FoldChange[1:10]
-heatmap_genes %in% rownames(ddset@assays@data@listData[["counts"]])
-#subset the counts that match the top ten from full data set
-heatmap_expression_data = ddset@assays@data@listData[["counts"]][rownames(ddset@assays@data@listData[["counts"]]) %in% heatmap_genes,]
+p <- ggplot(data = toplot,mapping= aes(x = type,stratum = group, alluvium = group, y = freq, fill = group))+
+  geom_flow(stat = "alluvium") +
+  geom_stratum(alpha = .5) +
+  scale_fill_manual(values = c("grey", "green", "red", "blue", "black"))  +
+  ggtitle("Changes in group memberships for different number of genes")
+show(p)
 
-install.packages("ComplexHeatmap")
 
-library(ComplexHeatmap)
-#set colors of two groups- infected and not infected
-col_fun = colorRamp2(c(0, 1), c("black", "white"))
-#generate heatmap and save to png
-png(filename = "gene_expr_heatmap.png",
-    width = 10, height = 8)
-Heatmap(heatmap_expression_data, bottom_annotation = HeatmapAnnotation(sample_type = ifelse(inf_data=="infected",
-                                                                                            0,
-                                                                                            1),
-                                                                       col = list(sample_type = col_fun),
-                                                                       show_annotation_name = FALSE),
-        name = "Gene\nExpression")

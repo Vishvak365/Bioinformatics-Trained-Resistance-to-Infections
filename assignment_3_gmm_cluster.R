dce5013e47dd9ae9f55174894d1abd7e9f10bf3e
Vishvak Seenichamy <vishvak365@gmail.com>
2021-10-21 16:48:29 -0400

    Finish Sankey Diagram for GMM

--- assignment_3_gmm_cluster.R
+++ assignment_3_gmm_cluster.R
@@ -23,3 +23,34 @@ clust_100 <- c(48, 44, 8)
 clust_1000 <- c(612, 336, 52) 
 clust_10000 <- c(6384, 3130, 486)
 
+all10 <- c( clust10)
+all100 <- c( clust100)
+all1000 <- c( clust1000)
+all10000 <- c( clust10000)
+clusternum <- c( "1", "2", "3")
+type <- c("gene10", "gene10","gene10",
+          "gene100","gene100","gene100",
+          "gene1000","gene1000","gene1000",
+          "gene10000","gene10000","gene10000")
+scalar1 <- function(x) {x / sqrt(sum(x^2))}
+scalar1(clusters10$size)
+normalize(clusters10$size)
+freq <- c(clusters10$size/10, clusters100$size/100,clusters1000$size/1000, clusters10000$size/10000)
+
+#k = 3
+group <- c("1", "2", "3",
+           "1", "2", "3",
+           "1", "2", "3",
+           "1", "2", "3",)
+
+#plotting
+toplot <- data.frame(type, freq, group)
+
+toplot
+
+p <- ggplot(data = toplot,mapping= aes(x = type,stratum = group, alluvium = group, y = freq, fill = group))+
+  geom_flow(stat = "alluvium") +
+  geom_stratum(alpha = .5) +
+  scale_fill_manual(values = c("grey", "green", "red"))  +
+  ggtitle("Changes in group memberships for different number of genes")
+show(p)
